e3a3f23b3b92081c7dc66e612486405984650867
Patrick Sheridan <sheridp2@gmail.com>
2021-06-23 13:28:31 -0700

    initial commit/ setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(3001, () => {
+  console.log("Listning on 3001");
+});
